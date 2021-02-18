46fd0fe6066a64fa2cda10050a4e1e9ba5bdd608
russa <aaron.russ@dfki.de>
2021-02-18 18:12:06 +0100

    improve performance: only try to remove mediaManager arg if mmir version is below 7

--- res/templates/compat-media-post-content-template.js
+++ res/templates/compat-media-post-content-template.js
@@ -1,13 +1,16 @@
 
     //backwards compatibility for media plugins that target mmir-lib < 7.x:
     //remove mediaManager instance from arguments
-    var __mediaManager__ = require('mmirf/mediaManager');
-    var modifiedArgs = [];
-    for(var i=0, offset=0, size=origArgs.length; i < size; ++i){
-      if(origArgs[i] === __mediaManager__){
-        ++offset;
-      } else {
-        modifiedArgs[i - offset] = origArgs[i];
+    var __mmir__ = require('mmirf/core');
+    if(!__mmir__.isVersion || __mmir__.isVersion(7, '<')){
+      var __mediaManager__ = require('mmirf/mediaManager');
+      var modifiedArgs = [];
+      for(var i=0, offset=0, size=origArgs.length; i < size; ++i){
+        if(origArgs[i] === __mediaManager__){
+          ++offset;
+        } else {
+          modifiedArgs[i - offset] = origArgs[i];
+        }
       }
+      origArgs = modifiedArgs;
     }
-    origArgs = modifiedArgs;
